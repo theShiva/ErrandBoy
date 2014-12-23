a77bfaa0b8dcf9913bc619aa21e0a7fbc21f6a91
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-22 23:11:56 -0800

    Added User POCO Entity

---
+++ src/ErrandBoy.Data/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace ErrandBoy.Data.Entities
+{
+    public class User
+    {
+        public virtual long UserId { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string UserName { get; set; }
+        public virtual byte[] Version { get; set; }
+    }
+}
