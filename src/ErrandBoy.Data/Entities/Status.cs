75e5a0e4c1d33df8a4521d3e225fd7f53f28e75f
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-22 23:06:57 -0800

    Removed unused directives across all Projects.

---
+++ src/ErrandBoy.Data/Entities/Status.cs
@@ -0,0 +1,11 @@
+﻿namespace ErrandBoy.Data.Entities
+{
+    public class Status
+    {
+        public virtual long StatusId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual int Ordinal { get; set; }
+        public virtual byte[] Version { get; set; }
+    }
+}
+                                                                         
