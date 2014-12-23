374d1cb5705baa83a9fca910a54986332e0778db
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-23 15:58:39 -0800

    Added Link POCO Service Model.

---
+++ src/ErrandBoy.Web.Api.Models/Link.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrandBoy.Web.Api.Models
+{
+    public class Link
+    {
+        public virtual string Rel { get; set; }
+        public virtual string Href { get; set; }
+        public virtual string Method { get; set; }
+    }
+}
