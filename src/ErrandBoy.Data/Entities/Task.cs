a7ad294ee03e53958ecbdb9a8216f3e124b42ec0
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-22 23:23:32 -0800

    Added Task POCO Entity.

---
+++ src/ErrandBoy.Data/Entities/Task.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrandBoy.Data.Entities
+{
+    public class Task
+    {
+        private readonly IList<User> _users = new List<User>();
+  
+        public virtual long TaskId { get; set; }
+        public virtual string Subject { get; set; }
+        public virtual DateTime? StartedOnDate { get; set; }
+        public virtual DateTime? DueOnDate { get; set; }
+        public virtual DateTime? CompletedOnDate { get; set; }
+        public virtual Status CurrentStatus { get; set; }
+        public virtual DateTime? CreatedOnDate { get; set; }
+        public virtual User CreatedBy { get; set; }
+
+        public virtual IList<User> Users
+        {
+            get { return _users; } 
+        }
+
+        public virtual byte[] Version { get; set; }
+
+    }
+}
