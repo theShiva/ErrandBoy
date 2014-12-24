bc5c227cca116d7be0c742f5c2d0ecc0c43808ac
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-23 16:29:20 -0800

    Created the User POCO in the Service Model (Web.Api.Models) Layer

---
+++ src/ErrandBoy.Web.Api.Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace ErrandBoy.Web.Api.Models
+{
+    public class User
+    {
+        private IList<Link> _links = new List<Link>();
+ 
+        public long UserId { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public IList<Link> Links { get; set; }
+
+        public void AddLink(Link link)
+        {
+            Links.Add(link);
+        }
+    }
+}
