62930cdf0796a7da3d3b1002379c18af19808b10
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-24 12:52:06 -0800

    Refactored Constraint Names in Task and TaskUser DB Table Definitions.

--- src/ErrandBoy.Database/Tables/TaskUser.sql
+++ src/ErrandBoy.Database/Tables/TaskUser.sql
@@ -3,7 +3,7 @@
 [TaskId] bigint NOT NULL,
 [UserId] bigint NOT NULL,
 [ts] rowversion NOT NULL,
-CONSTRAINT PK_TaskId_User_Id PRIMARY KEY (TaskId, UserId),
+CONSTRAINT PK_TaskUser PRIMARY KEY (TaskId, UserId),
 CONSTRAINT FK_TaskUser_User_UserId foreign key (UserId) references dbo.[User] (UserId),
 CONSTRAINT FK_TaskUser_Task_TaskId foreign key (TaskId) references dbo.Task (TaskId)
 )
