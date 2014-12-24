a1a0707a5b4e18cbfed91d506958322ccfa46556
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-23 16:38:01 -0800

    Refactored Links property to have a backing field.

--- src/ErrandBoy.Web.Api.Models/User.cs
+++ src/ErrandBoy.Web.Api.Models/User.cs
@@ -4,14 +4,18 @@ namespace ErrandBoy.Web.Api.Models
 {
     public class User
     {
-        private IList<Link> _links = new List<Link>();
+        private List<Link> _links;
  
         public long UserId { get; set; }
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        public IList<Link> Links { get; set; }
+        public List<Link> Links
+        {
+            get { return _links ?? (_links = new List<Link>()); }
+            set { _links = value; }
+        }
 
         public void AddLink(Link link)
         {
