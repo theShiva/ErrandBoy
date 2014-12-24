dc27bbb77fa8f04839c09d51767c74b43fd3a142
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-24 12:44:26 -0800

    Added the Task DB Table

---
+++ src/ErrandBoy.Database/Tables/Task.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Task] (
+[TaskId] BIGINT IDENTITY (1, 1) NOT NULL,
+[Subject] NVARCHAR (100) NOT NULL,
+[StartOnDate] DATETIME2 (7) NULL,
+[DueOnDate] DATETIME2 (7) NULL,
+[CompletedOnDate] DATETIME2 (7) NULL,
+[StatusId] BIGINT NOT NULL,
+[CreatedOnDate] DATETIME2 (7) NOT NULL,
+[CreatedByUserId] bigint NOT NULL,
+[ts] rowversion NOT NULL,
+CONSTRAINT PK_Task PRIMARY KEY CLUSTERED ([TaskId] ASC),
+CONSTRAINT FK_Status_StatusId FOREIGN KEY ([StatusId]) REFERENCES [dbo].[Status] ([StatusId]),
+CONSTRAINT FK_User_UserId FOREIGN KEY ([CreatedByUserId]) REFERENCES [dbo].[User] ([UserId])
+);
