9f3db42419dbc3facc87c7494ba5abee3253359a
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-24 12:50:22 -0800

    Added TaskUser DB Table.

---
+++ src/ErrandBoy.Database/Tables/TaskUser.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TaskUser]
+(
+[TaskId] bigint NOT NULL,
+[UserId] bigint NOT NULL,
+[ts] rowversion NOT NULL,
+CONSTRAINT PK_TaskId_User_Id PRIMARY KEY (TaskId, UserId),
+CONSTRAINT FK_TaskUser_User_UserId foreign key (UserId) references dbo.[User] (UserId),
+CONSTRAINT FK_TaskUser_Task_TaskId foreign key (TaskId) references dbo.Task (TaskId)
+)
