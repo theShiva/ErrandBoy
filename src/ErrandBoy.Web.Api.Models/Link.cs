0cd450279a6f40fa17e6d040bb600f5f7b7c7d06
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-23 16:02:10 -0800

    Removed unused directives across all projects in solution

--- src/ErrandBoy.Web.Api.Models/Link.cs
+++ src/ErrandBoy.Web.Api.Models/Link.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ErrandBoy.Web.Api.Models
+﻿namespace ErrandBoy.Web.Api.Models
 {
     public class Link
     {
