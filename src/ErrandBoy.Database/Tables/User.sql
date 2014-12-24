f27a3c9d5d7d06913315b0cadddb92f9772c1e61
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-24 12:17:45 -0800

    Attempting to fix the Revert issue, after I did a hard RESET at root in order to get git to start tracking updates in the .gitignore file (when I added the *.refactorlog line)

---
+++ src/ErrandBoy.Database/Tables/User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[UserId] BIGINT IDENTITY(1,1) NOT NULL, 
+    [UserName] NVARCHAR(50) NOT NULL, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [ts] ROWVERSION NOT NULL,
+	CONSTRAINT [PK_User] PRIMARY KEY ([UserId])
+)
