29cd9a9f9eebbe95184c73590a110e74682420f2
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-23 16:38:24 -0800

    Created the Task POCO Service Model (Web.Api.Model Layer)

---
+++ src/ErrandBoy.Web.Api.Models/Task.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrandBoy.Web.Api.Models
+{
+    public class Task
+    {
+        private List<Link> _links { get; set; }
+
+        public long TaskId { get; set; }
+        public string Subject { get; set; }
+        public DateTime? StartedOnDate { get; set; }
+        public DateTime? DueOnDate { get; set; }
+        public DateTime? CreatedOnDate { get; set; }
+        public DateTime? CompletedOnDate { get; set; }
+        public Status CurrentStatus { get; set; }
+        public List<User> Assignees { get; set; }
+
+        public List<Link> Links
+        {
+            get { return _links ?? (_links = new List<Link>()); }
+            set { _links = value; }
+        }
+
+        public void AddLink(Link link)
+        {
+            Links.Add(link);
+        }
+    }
+}
