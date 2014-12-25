72fcbdae3ad987b5da9f634a987bf8712a03f599
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-25 15:21:19 -0800

    Created ApiVersionConstraint class to implement Api Routing constraints so that the correct version of Api controller is used when Api is invoked from Api Clients.

---
+++ src/ErrandBoy.Web.Common/Routing/ApiVersionConstraint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Routing;
+
+namespace ErrandBoy.Web.Common.Routing
+{
+    public class ApiVersionConstraint //: IHttpRouteConstraint
+    {
+
+    }
+}
