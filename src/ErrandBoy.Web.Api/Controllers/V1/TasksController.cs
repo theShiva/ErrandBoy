57d7752802fed4a4c7e660259f952ef4371d0e50
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-25 14:21:39 -0800

    Introduced URI Versioning for Api Controllers and created empty TasksController in v1 of the Api

---
+++ src/ErrandBoy.Web.Api/Controllers/V1/TasksController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ErrandBoy.Web.Api.Controllers.V1
+{
+    public class TasksController : ApiController
+    {
+    }
+}
