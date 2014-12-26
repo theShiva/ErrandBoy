df08e4644ac6b23bcba471e27303c3b95f261a04
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-25 21:05:00 -0800

    Implemented ApiVersion1RoutePrefixAttribute class to encapsulate the 'api/v1' part of the route template so that we don’t have to copy and paste it over all of the web api controllers.

--- src/ErrandBoy.Web.Common/Routing/ApiVersion1RoutePrefixAttribute.cs
+++ src/ErrandBoy.Web.Common/Routing/ApiVersion1RoutePrefixAttribute.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Web.Http;
 
 namespace ErrandBoy.Web.Common.Routing
 {
-    class ApiVersion1RoutePrefixAttribute
+    public class ApiVersion1RoutePrefixAttribute : RoutePrefixAttribute
     {
+        private const string RouteBase = "api/{apiVersion:apiVersionConstraint(v1)}";
+        private const string PrefixRouteBase = RouteBase + "/";
+
+        public ApiVersion1RoutePrefixAttribute(string routePrefix)
+            : base(string.IsNullOrWhiteSpace(routePrefix) ? RouteBase : PrefixRouteBase + routePrefix)
+        {           
+        }
     }
 }
