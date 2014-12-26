f33947df40266690e2c34bf79e412ad637c115a9
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-26 11:04:47 -0800

    Created Empty NamespaceHttpControllerSelector class that will implement IHttpControllerSelector for custom controller selection because the framework only matches on the controller class name without regard to the controller class’ namespace.
    Related Blog Post :http://blogs.msdn.com/b/webdev/archive/2013/03/08/using-namespaces-to-version-web-apis.aspx

---
+++ src/ErrandBoy.Web.Common/NamespaceHttpControllerSelector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Dispatcher;
+
+namespace ErrandBoy.Web.Common
+{
+    public class NamespaceHttpControllerSelector : IHttpControllerSelector
+    {
+        public IDictionary<string, HttpControllerDescriptor> GetControllerMapping()
+        {
+            throw new NotImplementedException();
+        }
+
+        public HttpControllerDescriptor SelectController(HttpRequestMessage request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
