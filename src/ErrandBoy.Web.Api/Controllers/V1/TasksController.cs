d5aba6540523c232d0536254070c11af9ca22de0
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-25 21:11:17 -0800

    Added [ApiVersion1RoutePrefix("tasks")] attribute to the TaskController in v1 version of the Api. This is equivalent to saying [RoutePrefix("api/{apiVersion:apiVersionConstraint(v1)}/tasks")]

--- src/ErrandBoy.Web.Api/Controllers/V1/TasksController.cs
+++ src/ErrandBoy.Web.Api/Controllers/V1/TasksController.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using ErrandBoy.Web.Common.Routing;
 
 namespace ErrandBoy.Web.Api.Controllers.V1
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>The [ApiVersion1RoutePrefix("tasks")] attribute is equivalent to saying [RoutePrefix("api/{apiVersion:apiVersionConstraint(v1)}/tasks")]
+    /// </remarks>
+    [ApiVersion1RoutePrefix("tasks")]
     public class TasksController : ApiController
     {
     }
