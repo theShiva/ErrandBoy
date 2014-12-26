938f1906ee1f749f17d1b88b14353a755e1ca159
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-25 17:42:00 -0800

    Created ApiVersion1RoutePrefixAttribute class for Api Version 1 Routing.

---
+++ src/ErrandBoy.Web.Common/Routing/ApiVersion1RoutePrefixAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrandBoy.Web.Common.Routing
+{
+    class ApiVersion1RoutePrefixAttribute
+    {
+    }
+}
