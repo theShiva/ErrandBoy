2a5c34dbc161071c2461122a3096344f05cfc123
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-25 21:47:50 -0800

    Implemented AddTask(..) in api/v2 of TasksController.

--- src/ErrandBoy.Web.Api/Controllers/V2/TasksController.cs
+++ src/ErrandBoy.Web.Api/Controllers/V2/TasksController.cs
@@ -1,13 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Web.Http;
+using ErrandBoy.Web.Api.Models;
 
 namespace ErrandBoy.Web.Api.Controllers.V2
 {
+    [RoutePrefix("api/{apiVersion:apiVersionConstraint(v2)}/tasks")]
     public class TasksController : ApiController
     {
+        [Route("", Name = "AddTaskRouteV2")]
+        [HttpPost]
+        public Task AddTask(HttpRequestMessage requestMessage, Task newTask)
+        {
+            return new Task()
+            {
+                Subject = "In v2 : newTask.Subject = " + newTask.Subject
+            };
+        }
     }
 }
