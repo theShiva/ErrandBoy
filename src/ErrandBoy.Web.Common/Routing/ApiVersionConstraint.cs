7acdd269ca8ea46b09b44a67294925deac30461a
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-25 17:40:05 -0800

    Implemented the Match(...) method in the ApiVersionConstraint class.

--- src/ErrandBoy.Web.Common/Routing/ApiVersionConstraint.cs
+++ src/ErrandBoy.Web.Common/Routing/ApiVersionConstraint.cs
@@ -8,8 +8,23 @@ using System.Web.Http.Routing;
 
 namespace ErrandBoy.Web.Common.Routing
 {
-    public class ApiVersionConstraint //: IHttpRouteConstraint
+    public class ApiVersionConstraint : IHttpRouteConstraint
     {
+        public string AllowedVersion { get; private set; }
 
+        public ApiVersionConstraint(string allowedVersion)
+        {
+            AllowedVersion = allowedVersion.ToLowerInvariant();
+        }
+        public bool Match(System.Net.Http.HttpRequestMessage request, IHttpRoute route, string parameterName, IDictionary<string, object> values, HttpRouteDirection routeDirection)
+        {
+            object value;
+
+            if (values.TryGetValue(parameterName, out value) && value != null)
+            {
+                return AllowedVersion.Equals(value.ToString().ToLowerInvariant());
+            }
+            return false;
+        }
     }
 }
