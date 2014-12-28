237f722a685586e778dcf5d7e522579b27d1b6be
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-27 20:09:41 -0800

    Implemented the NamespaceHttpControllerSelector class constructor

--- src/ErrandBoy.Web.Common/NamespaceHttpControllerSelector.cs
+++ src/ErrandBoy.Web.Common/NamespaceHttpControllerSelector.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.Dispatcher;
 
@@ -8,6 +9,19 @@ namespace ErrandBoy.Web.Common
 {
     public class NamespaceHttpControllerSelector : IHttpControllerSelector
     {
+        private readonly HttpConfiguration _httpConfiguration;
+        private readonly Lazy<Dictionary<string, HttpControllerDescriptor>> _controllers;
+
+        public NamespaceHttpControllerSelector(HttpConfiguration configuration)
+        {
+            _httpConfiguration = configuration;
+            _controllers = new Lazy<Dictionary<string, HttpControllerDescriptor>>(InitializeControllerDictionary);
+        }
+
+        private Dictionary<string, HttpControllerDescriptor> InitializeControllerDictionary()
+        {
+            throw new NotImplementedException();
+        }
         public IDictionary<string, HttpControllerDescriptor> GetControllerMapping()
         {
             throw new NotImplementedException();
