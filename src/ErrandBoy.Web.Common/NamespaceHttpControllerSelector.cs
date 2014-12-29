e45de7ab3fa21fc02a5cd61d6a09b77fb02b7645
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-29 11:16:51 -0800

    Wrote 3 private helper methods in NamespaceHttpControllerSelector class

--- src/ErrandBoy.Web.Common/NamespaceHttpControllerSelector.cs
+++ src/ErrandBoy.Web.Common/NamespaceHttpControllerSelector.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.Dispatcher;
+using System.Web.Http.Routing;
+using NHibernate.Util;
 
 namespace ErrandBoy.Web.Common
 {
@@ -31,5 +34,38 @@ namespace ErrandBoy.Web.Common
         {
             throw new NotImplementedException();
         }
+
+        private string GetControllerName(IHttpRouteData httpRouteData)
+        {
+            var httpSubRoute = httpRouteData.GetSubRoutes().FirstOrDefault();
+            if (httpRouteData == null) return null;
+
+            var dataTokenValue = httpSubRoute.Route.DataTokens.First().Value;
+            if (dataTokenValue == null) return null;
+
+            var controllerName =
+                ((HttpActionDescriptor[]) dataTokenValue).First()
+                    .ControllerDescriptor.ControllerName.Replace("Controller", string.Empty);
+
+            return controllerName;
+        }
+
+        private T GetRouteVariable<T>(IHttpRouteData httpRouteData, string name)
+        {
+            object result;
+            if (httpRouteData.Values.TryGetValue(name, out result)) ;
+            {
+                return (T)result;
+            }
+            return default(T);
+        }
+
+        private string GetVersion(IHttpRouteData httpRouteData)
+        {
+            var httpSubRouteData = httpRouteData.GetSubRoutes().FirstOrDefault();
+            if (httpSubRouteData == null) return null;
+            return GetRouteVariable<string>(httpSubRouteData, "apiVersion");
+        }
+
     }
 }
