74df086eb953b5fb230fecf5c54639b18629af2f
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-30 12:19:06 -0800

    Created ILogManager Interface for Logging.

---
+++ src/ErrandBoy.Web.Api/Logging/ILogManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using log4net;
+
+namespace ErrandBoy.Web.Api.Logging
+{
+    public interface ILogManager
+    {
+        ILog GetLog(Type typeAssociatedWithRequestedLog);
+    }
+}
