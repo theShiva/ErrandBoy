a5af0c3e31ee40d21118fb093bf88431109476db
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-30 12:14:58 -0800

    Added binding for IDateTime <--> DateTimeAdapter.

--- src/ErrandBoy.Web.Api/App_Start/NinjectWebCommon.cs
+++ src/ErrandBoy.Web.Api/App_Start/NinjectWebCommon.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Web;
+using ErrandBoy.Common;
 using ErrandBoy.Web.Api;
 using Microsoft.Web.Infrastructure.DynamicModuleHelper;
 using Ninject;
@@ -61,6 +62,7 @@ namespace ErrandBoy.Web.Api
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            kernel.Bind<IDateTime>().To<DateTimeAdapter>().InSingletonScope();
         }        
     }
 }
