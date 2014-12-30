b2c299794f4574b836767926a0ad9cc9b4805cce
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-30 08:21:48 -0800

    Created DateTimeAdapter class that's a concrete implementation of the IDateTime interface.

---
+++ src/ErrandBoy.Common/DateTimeAdapter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ErrandBoy.Common
+{
+    public class DateTimeAdapter : IDateTime
+    {
+        public DateTime UtcNow
+        {
+            get { return DateTime.UtcNow; }
+        }
+    }
+}
