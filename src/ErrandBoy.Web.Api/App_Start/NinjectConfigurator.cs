9b87a1b11025ad34da071c2fd5d8269145901898
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-31 17:20:17 -0800

    Implemented the NinjectConfigurator class for DI Bindings.

---
+++ src/ErrandBoy.Web.Api/App_Start/NinjectConfigurator.cs
@@ -0,0 +1,29 @@
+﻿using ErrandBoy.Common;
+using ErrandBoy.Common.Logging;
+using log4net.Config;
+using Ninject;
+
+namespace ErrandBoy.Web.Api
+{
+    public class NinjectConfigurator
+    {
+        public void Configure(IKernel container)
+        {
+            AddBindings(container);
+        }
+
+        private void AddBindings(IKernel container)
+        {
+            ConfigureLog4Net(container);
+            container.Bind<IDateTime>().To<DateTimeAdapter>().InSingletonScope();
+        }
+
+        private void ConfigureLog4Net(IKernel container)
+        {
+            XmlConfigurator.Configure();
+
+            var logManager = new LogManagerAdapter();
+            container.Bind<ILogManager>().ToConstant(logManager);
+        }
+    }
+}
