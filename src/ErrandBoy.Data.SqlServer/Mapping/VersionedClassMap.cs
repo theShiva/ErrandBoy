5d11c51818e92b3569eeba8b2de6274bf137f221
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-01 14:41:36 -0800

    Implemented the constructor in VersionedClassMap Generic class.

--- src/ErrandBoy.Data.SqlServer/Mapping/VersionedClassMap.cs
+++ src/ErrandBoy.Data.SqlServer/Mapping/VersionedClassMap.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ErrandBoy.Data.Entities;
-using FluentNHibernate.Conventions;
+﻿using ErrandBoy.Data.Entities;
 using FluentNHibernate.Mapping;
 
 namespace ErrandBoy.Data.SqlServer.Mapping
 {
     public abstract class VersionedClassMap<T> : ClassMap<T> where T : IVersionedEntity
     {
-
+        protected VersionedClassMap()
+        {
+            Version(x => x.Version)
+                .Column("ts")
+                .CustomSqlType("Rowversion")
+                .Generated.Always()
+                .UnsavedValue("null");
+        }
     }
 }
