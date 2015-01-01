1a5056eaa74b2d23ca8a746528e48138ea6a7888
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-01 15:22:52 -0800

    Added NHibernate bindings and related private methods in NinjectConfigurator class.

--- src/ErrandBoy.Web.Api/App_Start/NinjectConfigurator.cs
+++ src/ErrandBoy.Web.Api/App_Start/NinjectConfigurator.cs
@@ -1,7 +1,14 @@
 ﻿using ErrandBoy.Common;
 using ErrandBoy.Common.Logging;
+using ErrandBoy.Data.SqlServer.Mapping;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
 using log4net.Config;
+using NHibernate;
+using NHibernate.Context;
 using Ninject;
+using Ninject.Activation;
+using Ninject.Web.Common;
 
 namespace ErrandBoy.Web.Api
 {
@@ -15,6 +22,8 @@ namespace ErrandBoy.Web.Api
         private void AddBindings(IKernel container)
         {
             ConfigureLog4Net(container);
+            ConfigureNHibernate(container);
+
             container.Bind<IDateTime>().To<DateTimeAdapter>().InSingletonScope();
         }
 
@@ -24,6 +33,31 @@ namespace ErrandBoy.Web.Api
 
             var logManager = new LogManagerAdapter();
             container.Bind<ILogManager>().ToConstant(logManager);
+        }
+
+        private void ConfigureNHibernate(IKernel container)
+        {
+            var sessionFactory = Fluently.Configure()
+                .Database(
+                    MsSqlConfiguration.MsSql2012.ConnectionString(
+                        c => c.FromConnectionStringWithKey("ErrandBoyDB")))
+            .CurrentSessionContext("web")
+            .Mappings(m => m.FluentMappings.AddFromAssemblyOf<TaskMap>())
+            .BuildSessionFactory();
+
+            container.Bind<ISessionFactory>().ToConstant(sessionFactory);
+            container.Bind<ISession>().ToMethod(CreateSession).InRequestScope();
+        }
+
+        private ISession CreateSession(IContext context)
+        {
+            var sessionFactory = context.Kernel.Get<ISessionFactory>();
+            if (!CurrentSessionContext.HasBind(sessionFactory))
+            {
+                var session = sessionFactory.OpenSession();
+                CurrentSessionContext.Bind(session);
+            }
+            return sessionFactory.GetCurrentSession();
         }
     }
 }
