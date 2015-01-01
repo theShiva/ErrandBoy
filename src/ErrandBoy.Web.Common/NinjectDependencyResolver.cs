fa31ef95c90f84b73459e4abdec93554e2baf7bc
Shiva Manjunath <shiva@haathimedia.com>
2014-12-31 17:31:25 -0800

    Implemented NinjectDependencyResolver class in ErrandBoy.Web.Common Project.

---
+++ src/ErrandBoy.Web.Common/NinjectDependencyResolver.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Dependencies;
+using Ninject;
+
+namespace ErrandBoy.Web.Common
+{
+    public sealed class NinjectDependencyResolver : IDependencyResolver
+    {
+        private readonly IKernel _container;
+
+        public NinjectDependencyResolver(IKernel container)
+        {
+            _container = container;
+        }
+
+        public IKernel Container
+        {
+            get { return _container; }
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _container.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _container.GetAll(serviceType);
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return this;
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
