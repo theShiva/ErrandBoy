4bd9ef427ddab71364e17d5b04e0ef5cef9bb475
Shiva Manjunath <theblogdoctor@gmail.com>
2014-12-31 19:20:52 -0800

    Added Generic VersionedClassMap class.

---
+++ src/ErrandBoy.Data.SqlServer/Mapping/VersionedClassMap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ErrandBoy.Data.Entities;
+using FluentNHibernate.Conventions;
+using FluentNHibernate.Mapping;
+
+namespace ErrandBoy.Data.SqlServer.Mapping
+{
+    public abstract class VersionedClassMap<T> : ClassMap<T> where T : IVersionedEntity
+    {
+
+    }
+}
