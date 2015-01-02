c3432beff55c8cfd386d0e08f0745213db3cdb36
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-01 21:09:59 -0800

    Created IActionTransactionHelper interface in ErrandBoy.Web.Common Project.

---
+++ src/ErrandBoy.Web.Common/IActionTransactionHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Http.Filters;
+
+namespace ErrandBoy.Web.Common
+{
+    public interface IActionTransactionHelper
+    {
+        void BeginTransaction();
+        void EndTransaction(HttpActionExecutedContext filterContext);
+        void CloseSession();
+    }
+}
