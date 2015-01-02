25479c67dd0498e029c3d94aa708298f096b3dc5
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-02 11:33:22 -0800

    Created ActionTransactionHelper that implements IActionTransactionHelper and coded the ctor.

---
+++ src/ErrandBoy.Web.Common/ActionTransactionHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Http.Filters;
+using NHibernate;
+
+namespace ErrandBoy.Web.Common
+{
+    public class ActionTransactionHelper : IActionTransactionHelper
+    {
+        private readonly ISessionFactory _sessionFactory;
+
+        public ActionTransactionHelper(ISessionFactory sessionFactory)
+        {
+            _sessionFactory = sessionFactory;
+        }
+
+        public void BeginTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndTransaction(HttpActionExecutedContext filterContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CloseSession()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
