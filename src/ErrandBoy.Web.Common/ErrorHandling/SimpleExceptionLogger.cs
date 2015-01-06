1c4190e621914c3381d9f6ca8d1478bd95ba8b22
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-06 07:19:39 -0800

    Added SimpleExceptionLogger with the single responsibility is to log exceptions. Although exceptions will also be logged using the tracing, this class has access to essential—information not available via the tracing infrastructure.

---
+++ src/ErrandBoy.Web.Common/ErrorHandling/SimpleExceptionLogger.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http.ExceptionHandling;
+using ErrandBoy.Common.Logging;
+using log4net;
+
+namespace ErrandBoy.Web.Common.ErrorHandling
+{
+    public class SimpleExceptionLogger : ExceptionLogger
+    {
+        private readonly ILog _log;
+
+        public SimpleExceptionLogger(ILogManager logManager)
+        {
+            _log = logManager.GetLog(typeof(SimpleExceptionLogger));
+        }
+
+        public override void Log(ExceptionLoggerContext context)
+        {
+            _log.Error("Unhandled exception", context.Exception);
+        }
+    }
+}
