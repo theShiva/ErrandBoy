712e19f3345883c9c7d75f5ac0156baab3f3ca83
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-06 07:32:57 -0800

    Created custom exception class RootObjectNotFoundException in ErrandBoy.Data Project, for exceptions that will be thrown by our various data access objects when the primary, or “aggregate root,” data object is not found.

---
+++ src/ErrandBoy.Data/Exceptions/RootObjectNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ErrandBoy.Data.Exceptions
+{
+    [Serializable]
+    public class RootObjectNotFoundException : Exception
+    {
+        public RootObjectNotFoundException(string message) : base(message)
+        {    
+        }
+    }
+}
