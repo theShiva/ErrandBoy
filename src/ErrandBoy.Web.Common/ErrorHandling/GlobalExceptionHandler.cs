6fb67add70cdd4ad600624c37bdbfa6a3cc6f0be
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-07 09:48:17 -0800

    Created SimpleErrorResult and stubbed out GlobalExceptionHandler for global error handling.

---
+++ src/ErrandBoy.Web.Common/ErrorHandling/GlobalExceptionHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using System.Web.Http.ExceptionHandling;
+using ErrandBoy.Data.Exceptions;
+
+namespace ErrandBoy.Web.Common.ErrorHandling
+{
+    public class GlobalExceptionHandler : ExceptionHandler
+    {
+
+    }
+}
