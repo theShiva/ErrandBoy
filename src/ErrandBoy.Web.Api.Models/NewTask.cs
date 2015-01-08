75a75015cfb839fe28e67987f632efc36096b580
Shiva Manjunath <shiva@haathimedia.com>
2015-01-08 11:49:11 -0800

    Created NewTask , a ViewModel / Service Model class.

---
+++ src/ErrandBoy.Web.Api.Models/NewTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ErrandBoy.Web.Api.Models
+{
+    public class NewTask
+    {
+        public string Subject { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? DueDate { get; set; }
+        public List<User> Assignees { get; set; }
+    }
+}
