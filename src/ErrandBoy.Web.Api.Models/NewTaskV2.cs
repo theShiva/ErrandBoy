f3d6f87115d1663f7a55c1e9a4405af2628726d4
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-09 11:49:53 -0800

    Created NewTaskV2 that requires only 1 Task Assignee. This is to demonstrate technique for avoiding OverPOSTing (i.e. API caller specifying values that are not expected either maliciously or unintentionally, and thereby updating other model properties into the database that the caller should not be allowed to update.

---
+++ src/ErrandBoy.Web.Api.Models/NewTaskV2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ErrandBoy.Web.Api.Models
+{
+    public class NewTaskV2
+    {
+        public string Subject { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? DueDate { get; set; }
+        public User Assignee { get; set; }
+    }
+}
