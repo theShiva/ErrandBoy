ac90418bd5ce5571864e45c384e49643270d2eb3
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-10 01:26:03 -0800

    Added the email fixer script. Use this to change email addresses on commits across entire repo.

---
+++ src/Scripts/git-fix-github-wrong-email-address-commits.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="theoldemail@address.com"
+CORRECT_NAME="Correct Name"
+CORRECT_EMAIL="thecorrectemail@address.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
