83b5e0134b34c5145235e4b030a93d00e68e21f8
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-12 10:12:47 -0800

    Created IWebUserSession interface that inherits from IUserSession interfrace.

---
+++ src/ErrandBoy.Web.Common/Security/IWebUserSession.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ErrandBoy.Common.Security;
+
+namespace ErrandBoy.Web.Common.Security
+{
+    public interface IWebUserSession : IUserSession
+    {
+        string ApiVersionInUse { get; }
+        Uri RequestUri { get; }
+        string HttpRequestMethod { get; }
+    }
+}
