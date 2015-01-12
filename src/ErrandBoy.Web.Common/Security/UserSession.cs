b40b5d6ca91d243ecdfe3db038c63cdc7718abc6
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-12 10:14:26 -0800

    Created UserSession class that implements IWebUserSession interface, and then stubbed outall the contracts.

---
+++ src/ErrandBoy.Web.Common/Security/UserSession.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ErrandBoy.Web.Common.Security
+{
+    public class UserSession : IWebUserSession
+    {
+        public string Firstname
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Lastname
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Username
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ApiVersionInUse
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public Uri RequestUri
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string HttpRequestMethod
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
