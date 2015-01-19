adf1594bbfcc30558144de4a167a130c615620fc
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-19 13:32:33 -0800

    Created NewTaskToTaskEntityAutoMapperTypeConfigurator to Map Task entity to the NewTask Service Model entity. Notice to use of Ignore to prevent overposting from the UI / Service layer.

---
+++ src/ErrandBoy.Web.Api/AutoMappingConfiguration/NewTaskToTaskEntityAutoMapperTypeConfigurator.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using ErrandBoy.Common.TypeMapping;
+using ErrandBoy.Web.Api.Models;
+using Task = ErrandBoy.Data.Entities.Task;
+
+namespace ErrandBoy.Web.Api.AutoMappingConfiguration
+{
+    public class NewTaskToTaskEntityAutoMapperTypeConfigurator : IAutoMapperTypeConfigurator
+    {
+        public void Configure()
+        {
+            Mapper.CreateMap<NewTask, Task>()
+                .ForMember(opt => opt.Version, x => x.Ignore())
+                .ForMember(opt => opt.CreatedBy, x => x.Ignore())
+                .ForMember(opt => opt.TaskId, x => x.Ignore())
+                .ForMember(opt => opt.CreatedOnDate, x => x.Ignore())
+                .ForMember(opt => opt.CompletedOnDate, x => x.Ignore())
+                .ForMember(opt => opt.CurrentStatus, x => x.Ignore())
+                .ForMember(opt => opt.Users, x => x.Ignore());
+        }
+    }
+}
