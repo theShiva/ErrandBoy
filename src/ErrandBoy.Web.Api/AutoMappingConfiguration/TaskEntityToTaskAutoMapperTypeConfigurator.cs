a76308bca5ef3d7ac9da8f60b6570546022e0035
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-19 13:45:14 -0800

    Created TaskEntityToTaskAutoMapperTypeConfigurator Mapper class.

---
+++ src/ErrandBoy.Web.Api/AutoMappingConfiguration/TaskEntityToTaskAutoMapperTypeConfigurator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using ErrandBoy.Common.TypeMapping;
+using ErrandBoy.Data.Entities;
+
+namespace ErrandBoy.Web.Api.AutoMappingConfiguration
+{
+    public class TaskEntityToTaskAutoMapperTypeConfigurator : IAutoMapperTypeConfigurator
+    {
+        public void Configure()
+        {
+            Mapper.CreateMap<Task, ErrandBoy.Web.Api.Models.Task>()
+                .ForMember(opt => opt.Links, x => x.Ignore())
+                .ForMember(opt => opt.Assignees, x => x.ResolveUsing<TaskAssigneesResolver>());
+        }
+    }
+}
