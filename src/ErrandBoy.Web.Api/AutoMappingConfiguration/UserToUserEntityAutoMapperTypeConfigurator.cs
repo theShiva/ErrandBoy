84c9ad7017ca28815593b81edcc725a167051835
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-19 13:46:10 -0800

    Created UserToUserEntityAutoMapperTypeConfigurator Mapper Class.

---
+++ src/ErrandBoy.Web.Api/AutoMappingConfiguration/UserToUserEntityAutoMapperTypeConfigurator.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ErrandBoy.Common.TypeMapping;
+using ErrandBoy.Web.Api.Models;
+
+namespace ErrandBoy.Web.Api.AutoMappingConfiguration
+{
+    public class UserToUserEntityAutoMapperTypeConfigurator : IAutoMapperTypeConfigurator
+    {
+        public void Configure()
+        {
+            Mapper.CreateMap<User, Data.Entities.User>()
+                .ForMember(opt => opt.Version, x => x.Ignore());
+        }
+    }
+}
