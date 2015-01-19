3b83604b1934e07717e0efedefea74f77037526e
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-19 13:29:22 -0800

    Created AutoMapperAdapter class and implemented IAutoMapper interface in it.

---
+++ src/ErrandBoy.Common/TypeMapping/AutoMapperAdapter.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace ErrandBoy.Common.TypeMapping
+{
+    public class AutoMapperAdapter : IAutoMapper
+    {
+        public T Map<T>(object objectToMap)
+        {
+            return Mapper.Map<T>(objectToMap);
+        }
+    }
+}
