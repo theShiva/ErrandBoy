10d040032fed501d3d0113e07643ef208ac187e3
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-19 13:44:52 -0800

    Created TaskAssigneesResolver for AutoMapper Mapping.

---
+++ src/ErrandBoy.Web.Api/AutoMappingConfiguration/TaskAssigneesResolver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using ErrandBoy.Common.TypeMapping;
+using ErrandBoy.Data.Entities;
+using ErrandBoy.Web.Common;
+using User = ErrandBoy.Web.Api.Models.User;
+
+namespace ErrandBoy.Web.Api.AutoMappingConfiguration
+{
+    public class TaskAssigneesResolver : ValueResolver<Task, List<User>>
+    {
+        public IAutoMapper AutoMapper
+        {
+            get { return WebContainerManager.Get<IAutoMapper>(); }
+        }
+        protected override List<User> ResolveCore(Task source)
+        {
+            return source.Users.Select(x => AutoMapper.Map<User>(x)).ToList();
+        }
+    }
+}
