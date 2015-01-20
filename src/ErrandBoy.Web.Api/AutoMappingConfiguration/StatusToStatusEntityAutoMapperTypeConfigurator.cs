c83e3b61676557b4133b9e2c1e431fd36633480d
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-20 12:03:24 -0800

    Created  StatusToStatusEntityAutoMapperTypeConfigurator Automapper mapping class.

---
+++ src/ErrandBoy.Web.Api/AutoMappingConfiguration/StatusToStatusEntityAutoMapperTypeConfigurator.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ErrandBoy.Common.TypeMapping;
+using ErrandBoy.Web.Api.Models;
+
+namespace ErrandBoy.Web.Api.AutoMappingConfiguration
+{
+    public class StatusToStatusEntityAutoMapperTypeConfigurator : IAutoMapperTypeConfigurator
+    {
+        public void Configure()
+        {
+            Mapper.CreateMap<Status, Data.Entities.Status>()
+            .ForMember(opt => opt.Version, x => x.Ignore());
+        }
+    }
+}
