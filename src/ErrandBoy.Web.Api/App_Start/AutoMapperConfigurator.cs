84312e6ae5f8fcfef4e4645f46bac997766c7530
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-21 11:13:28 -0800

    Created AutoMapperConfigurator for setting up the AutoMapper mapping on startup.

---
+++ src/ErrandBoy.Web.Api/App_Start/AutoMapperConfigurator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using ErrandBoy.Common.TypeMapping;
+
+namespace ErrandBoy.Web.Api
+{
+    public class AutoMapperConfigurator
+    {
+        public void Configure(IEnumerable<IAutoMapperTypeConfigurator> autoMapperTypeConfigurations)
+        {
+            autoMapperTypeConfigurations.ToList().ForEach(x => x.Configure());
+            Mapper.AssertConfigurationIsValid();
+        }
+    }
+}
