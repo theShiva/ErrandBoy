2c2ff2a14fc28a10ba43f354efab1eee6fcee436
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-23 11:33:56 -0800

    Declared IAddTaskMaintenanceProcessor interface for adding new tasks.

---
+++ src/ErrandBoy.Web.Api/MaintenanceProcessing/IAddTaskMaintenanceProcessor.cs
@@ -0,0 +1,10 @@
+﻿using ErrandBoy.Web.Api.Models;
+using Task = System.Threading.Tasks.Task;
+
+namespace ErrandBoy.Web.Api.MaintenanceProcessing
+{
+    public interface IAddTaskMaintenanceProcessor
+    {
+        Task AddTask(NewTask newTask);
+    }
+}
