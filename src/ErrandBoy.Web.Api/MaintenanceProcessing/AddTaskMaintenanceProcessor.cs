e69c62e776ea1c67d57a3740f6107f9ecb293fe8
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-23 11:37:57 -0800

    Created AddTaskMaintenanceProcessor as a concrete implementation of IAddTaskMaintenanceProcessor interface for persisting new Tasks to database.

---
+++ src/ErrandBoy.Web.Api/MaintenanceProcessing/AddTaskMaintenanceProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ErrandBoy.Common.TypeMapping;
+using ErrandBoy.Data.QueryProcessors;
+using ErrandBoy.Web.Api.Models;
+using Task = System.Threading.Tasks.Task;
+
+namespace ErrandBoy.Web.Api.MaintenanceProcessing
+{
+    public class AddTaskMaintenanceProcessor : IAddTaskMaintenanceProcessor
+    {
+        private readonly IAutoMapper _autoMapper;
+        private readonly IAddTaskQueryProcessor _queryProcessor;
+
+        public AddTaskMaintenanceProcessor(IAddTaskQueryProcessor queryProcessor,
+        IAutoMapper autoMapper)
+        {
+            _queryProcessor = queryProcessor;
+            _autoMapper = autoMapper;
+        }
+
+        public Task AddTask(NewTask newTask)
+        {
+            var taskEntity = _autoMapper.Map<Data.Entities.Task>(newTask);
+
+            _queryProcessor.AddTask(taskEntity);
+
+            var task = _autoMapper.Map<Task>(taskEntity);
+
+            return task;
+        }
+    }
+}
