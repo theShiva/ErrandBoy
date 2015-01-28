c42bc341aa8aa48466ecc04f167397ae21c5cacb
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-28 14:38:57 -0800

    Added ILinkContaining interface.

---
+++ src/ErrandBoy.Web.Api.Models/ILinkContaining.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ErrandBoy.Web.Api.Models
+{
+    public interface ILinkContaining
+    {
+        List<Link> Links { get; set; }
+        void AddLink(Link link);
+    }
+}
