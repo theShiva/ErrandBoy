67ab37e4ec0536e8821a65c975ea008ede39e2a9
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-29 21:21:32 -0800

    Added Constants class in ErrandBoy.Common project for maintaining application-wide constants as static classes.

---
+++ src/ErrandBoy.Common/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace ErrandBoy.Common
+{
+    public static class Constants
+    {
+        public static class MediaTypeNames
+        {
+            public const string ApplicationXml = "application/xml";
+            public const string TextXml = "text/xml";
+            public const string ApplicationJson = "application/json";
+            public const string TextJson = "text/json";
+        }
+    }
+}
