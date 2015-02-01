dda938e97cce6760ec9a3e2c6047abee59f5076a
Shiva Manjunath <theblogdoctor@gmail.com>
2015-01-31 18:40:17 -0800

    Added CommonLinkRelValues and CommonRoutingDefinitions static classes for the constants

--- src/ErrandBoy.Common/Constants.cs
+++ src/ErrandBoy.Common/Constants.cs
@@ -20,5 +20,19 @@
             public const string PageNumber = "pageNumber";
             public const string PageSize = "pageSize";
         }
+        public static class CommonLinkRelValues
+        {
+            public const string Self = "self";
+            public const string All = "all";
+            public const string CurrentPage = "currentPage";
+            public const string PreviousPage = "previousPage";
+            public const string NextPage = "nextPage";
+        }
+        public static class CommonRoutingDefinitions
+        {
+            public const string ApiSegmentName = "api";
+            public const string ApiVersionSegmentName = "apiVersion";
+            public const string CurrentApiVersion = "v1";
+        }
     }
 }
