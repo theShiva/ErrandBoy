6b4c9ed5cff708b40172f410d165e1986eea07b3
Shiva Manjunath <theblogdoctor@gmail.com>
2015-02-03 11:18:50 -0800

    Spaced out the various constant classes in the ErrandBoy.Common.Constants parent Static class.

--- src/ErrandBoy.Common/Constants.cs
+++ src/ErrandBoy.Common/Constants.cs
@@ -9,17 +9,20 @@
             public const string ApplicationJson = "application/json";
             public const string TextJson = "text/json";
         }
+        
         public static class Paging
         {
             public const int MinPageSize = 1;
             public const int MinPageNumber = 1;
             public const int DefaultPageNumber = 1;
         }
+        
         public static class CommonParameterNames
         {
             public const string PageNumber = "pageNumber";
             public const string PageSize = "pageSize";
         }
+        
         public static class CommonLinkRelValues
         {
             public const string Self = "self";
@@ -28,21 +31,25 @@
             public const string PreviousPage = "previousPage";
             public const string NextPage = "nextPage";
         }
+
         public static class CommonRoutingDefinitions
         {
             public const string ApiSegmentName = "api";
             public const string ApiVersionSegmentName = "apiVersion";
             public const string CurrentApiVersion = "v1";
         }
+
         public static class SchemeTypes
         {
             public const string Basic = "basic";
         }
+
         public static class RoleNames
         {
             public const string Manager = "Manager";
             public const string SeniorWorker = "SeniorWorker";
             public const string JuniorWorker = "JuniorWorker";
         }
+
     }
 }
